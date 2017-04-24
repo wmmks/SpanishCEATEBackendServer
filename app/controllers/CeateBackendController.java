ab3fae6fb755dca5c44fb15bf75e4c5b45ccb85c
roye <roye1202@hotmail.com.tw>
2017-04-24 19:56:40 +0800

    Remove useless class

---
+++ app/controllers/CeateBackendController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import play.mvc.*;
+
+import views.html.*;
+
+/**
+ * Created by roye on 2017/4/24.
+ */
+public class CeateBackendController extends Controller{
+
+    public Result HelloWorld()
+    {
+
+        return ok("HelloWorld");
+    }
+}
